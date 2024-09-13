e1963be07c6fe18797083d88f467dae3ea954995
Bogdan Zaharkin <bogdanzaharkin8@gmail.com>
2024-09-13 19:46:04 +0300

    Pointer

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import "fmt"
 
-func main() {
-	var hello_string string
+func createHello() (result *string) {
+	str := "Hello GO"
+	result = &str
+	return
+}
 
-	hello_string = "Hello GO"
-	fmt.Println(hello_string)
+func main() {
+	hello := createHello()
+	fmt.Println(*hello)
 }
